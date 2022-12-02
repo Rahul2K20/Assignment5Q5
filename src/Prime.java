f6e92e006167b1abaea844d04e883f48ee4d165f
Rahul Nagarajan <63788480+Rahul2K20@users.noreply.github.com>
2022-12-01 20:42:25 -0800

    Added JUnit Testing
    Java program to check whether a number is prime

---
+++ src/Prime.java
@@ -0,0 +1,20 @@
+public class Prime {
+ public boolean isPrime(int number) {
+	 
+ boolean prime = false; 
+ for(int i=2; i<=number/2; ++i) {
+	 if(number%i==0) {
+		 prime = true;
+		 break;
+	 }
+ }
+ if (!prime) {
+	 return true;
+ }
+ else {
+	 return false; 
+ }
+}
+}
+
+
---
+++ src/PrimeTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PrimeTest {
+	
+ @Test
+ public void test_2() {
+	 Prime number = new Prime(); 
+	 assertTrue(number.isPrime(2));
+ }
+ 
+ @Test 
+ public void test_3() {
+	 Prime number = new Prime();
+	 assertTrue(number.isPrime(3));
+ }
+ 
+ @Test
+ public void test_4() {
+	 Prime number = new Prime();
+	 assertFalse(number.isPrime(4));
+ }
+ 
+ @Test 
+ public void test_5() {
+	 Prime number = new Prime();
+	 assertTrue(number.isPrime(5));
+ }
+ 
+ @Test
+ public void test_6() {
+	 Prime number = new Prime();
+	 assertFalse(number.isPrime(6));
+ }
+ 
+}
